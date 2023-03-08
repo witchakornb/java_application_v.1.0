b84f5c22a99828af3e7ac0eb5ab762cd4349e83f
witchakorn boonprakom <119865344+witchakornb@users.noreply.github.com>
2023-03-08 22:21:32 +0700

    Home page

--- src/main/java/com/login/application/HelloController.java
+++ src/main/java/com/login/application/HelloController.java
@@ -1,14 +1,23 @@
 package com.login.application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
 }
